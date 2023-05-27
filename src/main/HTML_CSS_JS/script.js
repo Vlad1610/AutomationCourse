5b491f4c42a2f249c9831ed2ee9d5b0f3310aab8
Vlad1610 <constantin_vlad1998@yahoo.com>
2023-05-27 14:05:37 +0300

    HTML, CSS and JS

---
+++ src/main/HTML_CSS_JS/script.js
@@ -0,0 +1,19 @@
+function clickMe() {
+    var var1 = "abc";
+    var var2 = "5";
+    var var3 = "false";
+    //alert("Hello!");
+    readInput();
+}
+
+function readInput() {
+    var usernameText = document.getElementById("username").value;
+    //alert(usernameText)
+    document.getElementById("result").innerHTML = "the username is " + usernameText;
+
+    if(usernameText.length > 10) {
+        alert("The max size of 10 has been exceeded");
+        document.getElementById("username").style.color="red";
+    }
+}
+

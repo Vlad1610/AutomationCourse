4d196054259f642a2ab22e37f057c887083b944a
Vlad1610 <constantin_vlad1998@yahoo.com>
2023-06-20 20:10:51 +0300

    Changed text output for Homework03

--- src/main/java/ro/sit/homework/Homework03/Library/Library.java
+++ src/main/java/ro/sit/homework/Homework03/Library/Library.java
@@ -8,7 +8,7 @@ public class Library {
     public static void main(String[] args) {
         Author author = new Author("Mircea Eliade", "mircea.eliade@gmail.com");
         Book book = new Book("Maytrei ", 1933, author, 12.50);
-        System.out.println("Book " + book.getName() + book.getPrice() + " RON by " + author.getName() +
-                " published in " + book.getYear());
+        System.out.println("Book " + book.getName() + "(" + book.getPrice() + " RON), by " + author.getName() +
+                ", published in " + book.getYear());
     }
 }

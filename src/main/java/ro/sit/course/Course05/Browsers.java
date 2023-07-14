50514b1b7d0d637e5ea3114c102c96299df40c1d
Vlad1610 <constantin_vlad1998@yahoo.com>
2023-07-14 21:27:23 +0300

    added class work

---
+++ src/main/java/ro/sit/course/Course05/Browsers.java
@@ -0,0 +1,7 @@
+package ro.sit.course.Course05;
+
+public enum Browsers {
+    CHROME,
+    FIREFOX,
+    EDGE
+}
---
+++ src/main/java/ro/sit/course/course06/MyCustomException.java
@@ -0,0 +1,7 @@
+package ro.sit.course.course06;
+
+public class MyCustomException extends Exception {
+    public MyCustomException(String message) {
+        super(message);
+    }
+}
